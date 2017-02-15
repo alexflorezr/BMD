2b836a70cb8d3702a8d4854f735af7e599ffd5eb
Alexander Florez Rodriguez <alexflorezr@gmail.com>
2017-02-15 15:29:34 +0100

    Added function barplot for the genbank records wiith and w/out coordinates

--- museum_bplot.R
+++ museum_bplot.R
@@ -11,10 +11,22 @@ museum_bplot <- function(museum_id_table){
         museum_bplot$Coor_records <- museum_coor_bplot$Records[museum_match]
         temp_df <- as.data.frame(matrix(nrow = length(unique)))
         # the plot
-        barplot(museum_bplot$Records, decreasing = T,las=2, ylim = c(0,8500),
-                names.arg = museum_bplot$Museum, las=2, main="Museums")
-        barplot(museum_bplot$Coor_records, decreasing = T,las=2, ylim = c(0,8500),
-                add = T, col="red")
-        abline(h=seq(0,9000, 500), lty=3)
+        par(mar=c(9,8,8,8))
+        pdf("museum_id_records.pdf")
+        barplot(museum_bplot$Records,las=2, ylim = c(0,8500), xlim=c(1,40),
+                names.arg = museum_bplot$Museum, las=2, col="#C1CDCD",
+                border = NA,cex.names = 0.8)
+        barplot(museum_bplot$Coor_records,las=2, ylim = c(0,8500),
+                add = T, col="#CD853F", border = NA)
+        mtext(side=3, line=2, "Museums with more than 400 records in GenBank", cex=1.5)
+        total <- sum(museum_bplot$Records)
+        mtext(side=3, line=1, paste(total, "records in total"))
+        mtext(side=1, line=4.5, cex=1.2, "Museum short name")
+        mtext(side=2, line=4, cex=1.2, "Number of records")
+        abline(h=seq(0,9000, 1000), lty=3)
+        legend(20, 8000, legend = c("Voucher only", "Voucher and coordinates"),
+               fill = c("#C1CDCD", "#CD853F"), bty = "n" , border = "white",
+               x.intersp = 0.2, y.intersp = 0.7)
+        dev.off()
 }
 museum_bplot(museum_id_uniq)
