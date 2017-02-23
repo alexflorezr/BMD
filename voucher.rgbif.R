29e230fb40e83b0115e2bb6d243b8496c9aab0bb
Alexander Florez Rodriguez <alexflorezr@gmail.com>
2017-02-23 18:38:44 +0100

    eliminated the for through the museums, added mus vector for occ_search

--- voucher.rgbif.R
+++ voucher.rgbif.R
@@ -12,7 +12,6 @@ voucher.rgbif <- function(database, threshold){
         DB_thold$Sp_bi <- gsub("_", " ", gsub("([A-Za-z]+_[A-Za-z]+).*", "\\1",DB_thold$Species))
         mus_sp <- unique(DB_thold$Sp_bi)
         list_sp_mus <-list()
-        print(mus_sp)
         for(sp in seq_along(mus_sp)){
                 sci_name <- mus_sp[sp]
                 tmp_DB <- subset(DB_thold, DB_thold$Sp_bi == sci_name)
@@ -20,21 +19,28 @@ voucher.rgbif <- function(database, threshold){
                 sp_hits <- data.frame(matrix(nrow=0, ncol=7))
                 colnames(sp_hits) <- c("Species", "Gbif_ID", "Lat", "Long", "Inst_code",
                                        "Catalog_num", "Verb_Locality")
-                for (mus in seq_along(mus_ID_sp)){
-                        inst_code <- mus_ID_sp[mus]
-                        tmp_search <- occ_search(scientificName = sci_name, 
-                                                 institutionCode = inst_code)
-                        if (tmp_search$meta$count > 0){
-                                Catalog_num <- tmp_search$data$catalogNumber
-                                hit <- which(grepl(tmp_DB$Number_id, tmp_catalog_num))
-                                Species <- tmp_search$data$species[hit]
-                                Gbif_ID <- tmp_search$data$gbifID[hit]
-                                Lat <- tmp_search$data$decimalLatitude[hit]
-                                Long <- tmp_search$data$decimalLongitude[hit]
-                                Inst_code <- tmp_search$data$institutionCode[hit]
-                                Catalog_num <- tmp_search$data$catalogNumber[hit]
-                                Verb_Locality <- tmp_search$data$locality[hit]
-                                hit_tbl <- cbind(Species, Gbif_ID, Lat, 
+                tmp_search <- occ_search(scientificName = sci_name, institutionCode = mus_ID_sp)
+                tmp_names <- names(tmp_search)
+                for (mus in seq_along(tmp_names)){
+                        # if the museum has zero hits dont loop through it
+                        # Focus now in the hits with lat and long, only chech hits with lat != NA
+                        # if the hit has lat and long then match the catalog number and extract the positive hits
+                        tmp_search[mus][[tmp_names[mus]]]$data$catalogNumber
+                        tmp_search[names(tmp_search) == tmp_names[mus]] 
+                        
+                }
+                if (tmp_search$meta$count > 0){
+                        Catalog_num <- tmp_search$data$catalogNumber
+                        hit <- match(tmp_DB$Number_id, gsub(".*([0-9]*)*.*", "\\1", Catalog_num))
+                        hit <- as.vector(na.exclude(hit))
+                        Species <- tmp_search$data$species[hit]
+                        Gbif_ID <- tmp_search$data$gbifID[hit]
+                        Lat <- tmp_search$data$decimalLatitude[hit]
+                        Long <- tmp_search$data$decimalLongitude[hit]
+                        Inst_code <- tmp_search$data$institutionCode[hit]
+                        Catalog_num <- tmp_search$data$catalogNumber[hit]
+                        Verb_Locality <- tmp_search$data$locality[hit]
+                        hit_tbl <- cbind(Species, Gbif_ID, Lat, 
                                                  Long, Inst_code, 
                                                  Catalog_num, Verb_Locality)
                                 sp_hits <- rbind(sp_hits, hit_tbl)
@@ -43,11 +49,11 @@ voucher.rgbif <- function(database, threshold){
                 if(dim(sp_hits)[1] > 0){
                         list_sp_mus[[sp]] <- sp_hits
                 }
-                print(sp)
+                print(paste(sp, " out of ", length(mus_sp)))
         }
         list_sp_mus
 }
 random_rows <- sample(1:dim(database)[1], size = 100, replace = F)
-kk <- voucher.rgbif(database = database[random_rows,], 1)
+kk <- voucher.rgbif(database = database, 1000)
 DB <- database[random_rows,]
   
